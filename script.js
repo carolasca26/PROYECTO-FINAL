995aacaa2f37fb926fa9de5e73276946789c14d7
carolasca26 <lascacarolina@gmail.com>
2024-05-15 21:55:54 -0300

    tarea clase 10

---
+++ script.js
@@ -0,0 +1,21 @@
+let nombre = "Pedro";
+let apellido = "Gomez";
+let years = 30;
+console.log( nombre + " " + apellido);
+
+function AgregarDiv() {
+
+}
+
+let numberone = 120;
+let numbertwo = 20;
+let resultadoSuma = numberone + numbertwo;
+let resultadoResta = numberone - numbertwo;
+let resultadoMulti = numberone * numbertwo;
+let resultadoDivision = numberone / numbertwo;
+
+
+console.log("Suma: ", resultadoSuma)
+console.log("Resta: ", resultadoResta)
+console.log("Multiplicacion: ", resultadoMulti)
+console.log("Division: ", resultadoDivision)
